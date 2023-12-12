9ab8b71494bc6cffa284dbdd8228f30dd990b9d6
polan233 <1292437680@qq.com>
2023-12-12 20:19:34 +0800

    feat(app.py):加入新接口，存储预设，获得所有预设

--- back-end/app.py
+++ back-end/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import sqlite3
 import datetime
 import subprocess
@@ -16,6 +16,19 @@ cursor.execute('''
         code TEXT,
         run_datetime TEXT,
         result TEXT
+    )
+''')
+# 创建表格
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS presets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(50),
+        pre_code TEXT,
+        interpreter_path TEXT,
+        script_path TEXT,
+        parameters TEXT,
+        final_code TEXT,
+        description TEXT
     )
 ''')
 conn.commit()
@@ -56,6 +69,59 @@ def execute_code():
     conn.close()
 
     return result
+
+@app.route('/savePreset', methods=['POST'])
+def save_preset():
+    # 获取请求参数
+    name = request.args.get('name')
+    pre_codes = request.args.getlist('preCode')
+    interpreter_path = request.args.get('interpreterPath')
+    script_path = request.args.get('scriptPath')
+    parameters = request.args.getlist('parameters')
+    final_code = request.args.get('finalCode')
+    description = request.args.get('description')
+
+    # 连接数据库
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+
+    # 将数据插入数据库表中
+    cursor.execute('INSERT INTO presets (name, pre_code, interpreter_path, script_path, parameters,final_code,description) VALUES (?, ?, ?, ?, ?,?,?)',
+                   (name, '\n'.join(pre_codes), interpreter_path, script_path, ' '.join(parameters), final_code,description))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': '预设保存成功'})
+
+@app.route('/getPresets', methods=['GET'])
+def get_presets():
+    # 连接数据库
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # 查询所有预设项目
+    cursor.execute('SELECT * FROM presets')
+    presets = cursor.fetchall()
+
+    # 将查询结果转换为字典列表
+    presets_list = []
+    for preset in presets:
+        preset_dict = {
+            'id': preset[0],
+            'name': preset[1],
+            'pre_code': preset[2],
+            'interpreter_path': preset[3],
+            'script_path': preset[4],
+            'parameters': preset[5],
+            'final_code': preset[6],
+            'description': preset[7]
+        }
+        presets_list.append(preset_dict)
+
+    conn.close()
+
+    return jsonify(presets_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
